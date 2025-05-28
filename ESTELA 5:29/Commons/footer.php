0d2f7a7dff6a26e7c0fc7468263dfa597d5ba752
Y-Tsuru12 <all.one.wants@outlook.jp>
2025-05-29 04:19:11 +0900

    Add files via upload

---
+++ ESTELA 5:29/Commons/footer.php
@@ -0,0 +1,18 @@
+<!--  共通フッター -->
+<footer>
+  <div class="footer-container">
+    <div class="store-details">
+      <p class="store-name english">estela</p>
+      <p class="store-address">兵庫県 尼崎市 東園田町 5丁目142-1 法界寺ビル2F</p>
+      <p>TEL : 070-9090-5646 </p>
+    </div>
+    <div class="other-link-pc">
+      <a class="instagram" href="https://www.instagram.com/estela_2025_0402?igsh=MXM5dm5leWhsbTN6eg==">
+        <span class="iconify" data-icon="mdi:instagram"></span>
+      </a>
+      <a class="hotpepper" href="https://beauty.hotpepper.jp/slnH000750466/">HOT PEPPER<br>Beauty</a>
+    </div>
+    <p class="copyright english">&copy;estela</p>
+  </div>
+</footer>
+<!--  /共通フッター -->
